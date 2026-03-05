4e06392c5b7214d4fb8d436abffbd2d6666d1a48
Felipe-Alves-de-Souza <felipe.ssouza@sptech.school>
2026-03-04 23:22:23 -0300

    Site institucional estático pronto

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var botaoVideo = document.getElementById("verVideo");
+    var conectaDiv = document.querySelector(".conecta");
+
+    botaoVideo.addEventListener("click", function () {
+
+        conectaDiv.innerHTML = `
+            <video id="videoConecta" controls autoplay width="400" style="padding: 20px 20px;">
+                <source src="assets/imgs/videoconecta.mp4" type="video/mp4">
+                Seu navegador não suporta vídeo.
+            </video>
+        `;
+
+        var video = document.getElementById("videoConecta");
+
+        video.addEventListener("ended", function () {
+            conectaDiv.innerHTML = `
+                <img src="assets/imgs/conecta_logo_transparente.png" alt="conecta">
+            `;
+        });
+
+    });
+
+});
 var menu = document.querySelector(".menu");
 var ul = document.querySelector('.ul');
 var links = document.querySelectorAll(".ul a");
+var video = document.querySelectorAll(".clique");
+var logo = document.querySelectorAll(".conecta")
+var botaoVideo = document.getElementById("verVideo");
+
 
 menu.addEventListener('click', ()=>{
   
@@ -21,3 +50,6 @@ for (var i = 0; i < links.length; i++) {
         document.querySelector(".menu img").src = "assets/icon/menu.png";
     });
 }
+
+
+
