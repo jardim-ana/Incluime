0b34d6979387cc47021f564ca40717329594c7e2
Felipe-Alves-de-Souza <felipe.ssouza@sptech.school>
2026-03-28 21:11:15 -0300

    adição: tela de questionário e dashboard com sidebar

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -2,3 +2,12 @@ document.getElementById('abrir_icon').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
+var logoutButton = document.getElementById("logout_button");
+
+    logoutButton.addEventListener("click", function () {
+        var confirmarSaida = confirm("Tem certeza que deseja sair?");
+
+        if (confirmarSaida) {
+            window.location.href = "index.html";
+        }
+    });
