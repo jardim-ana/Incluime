01536ba161f81e4999479eebdc71268a0607799b
gMedeiros23 <gabriel.nascimento@sptech.school>
2026-04-29 10:37:19 -0300

    Melhoria: Adicionado atributo faltante na autenticacao do login

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 function log(mensagem, nivel) {
     var instrucao = `
-        INSERT INTO logss (acao, nivel)
+        INSERT INTO logss (acao, tipo)
         VALUES ('${mensagem}', '${nivel}');
     `;
     return database.executar(instrucao);
