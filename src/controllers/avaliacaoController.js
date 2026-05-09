670ec03e0322683a26cfae4199cb6e9213926f5e
Felipe-Alves-de-Souza <felipe.ssouza@sptech.school>
2026-05-09 01:51:23 -0300

    adição: configuração do slack

--- src/controllers/avaliacaoController.js
+++ src/controllers/avaliacaoController.js
@@ -1,4 +1,5 @@
 var avaliacaoModel = require("../models/avaliacaoModel");
+const { execFile } = require("child_process");
 
 function cadastrar(req, res) {
     var nota = req.body.notaServer;
@@ -16,24 +17,51 @@ function cadastrar(req, res) {
     }
     else {
         avaliacaoModel.cadastrar(nota, comentario, idUsuario)
-            .then(
-                function(resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function(erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro no banco ao realizar o cadastro! Erro:",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+            .then(function(resultado) {
+
+                enviarNotificacaoSlack(nota, comentario, idUsuario);
+
+                res.json(resultado);
+            })
+            .catch(function(erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro no banco ao realizar o cadastro! Erro:",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            });
     }
+}
+
+function enviarNotificacaoSlack(nota, comentario, idUsuario) {
+    const caminhoJar = "ETL-ApachePOI/target/etl-apachepoi-1.0-SNAPSHOT.jar";
+
+    execFile(
+        "java",
+        [
+            "-cp",
+            caminhoJar,
+            "incluime.conectamais.NotificacaoSlack",
+            String(nota),
+            comentario || "Sem comentário",
+            String(idUsuario)
+        ],
+        function(error, stdout, stderr) {
+            if (error) {
+                console.log("Erro ao chamar Java para enviar Slack:", error.message);
+                return;
+            }
+
+            if (stderr) {
+                console.log("Erro no Java/Slack:", stderr);
+            }
+
+            console.log("Retorno Java/Slack:", stdout);
+        }
+    );
 }
 
 module.exports = {
     cadastrar
 };
-    
--- src/models/avaliacaoModel.js
+++ src/models/avaliacaoModel.js
@@ -4,7 +4,7 @@ function cadastrar(comentario, nota, idUsuario) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", comentario, nota, idUsuario);
 
     var instrucao = `
-        INSERT INTO avaliacao (descricao, nota, dtComentario, usuario_id, escola_id) VALUES ('${nota}', '${comentario}', default, '${idUsuario}', null);
+        INSERT INTO avaliacao (descricao, nota, dtComentario, usuario_id, escola_id) VALUES ('${nota}', '${comentario}', default, '${idUsuario}', 1);
     `;
     console.log("executando instrução: \n" + instrucao);
     return database.executar(instrucao);
